44303ad60a405e5f9c4e77dc7eae08a2cb0e5201
Mehmet Gündaş <mehmetgundass@gmail.com>
2025-03-25 02:46:52 +0300

    added resume and done some updates

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
                     <h1 className='text-2xl font-medium'>{db.profile.name}</h1>
                     <p className='text-2xl text-cyan-400 font-light'>{db.profile.title}</p>
                     <p className='text-2xl font-light'>{db.profile.description}</p>
-                    <button className='get-resume-button'>Get my resume</button>
+                    <a href='/resume.pdf' className='get-resume-button'>Get my resume</a>
                 </div>
                 <div className='flex-1/4 p-6 flex items-center justify-center'>
                     <img src={db.profile.image} alt={db.profile.name} />
